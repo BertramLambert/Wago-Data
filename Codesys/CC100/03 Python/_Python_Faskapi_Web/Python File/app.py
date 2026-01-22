ecc8e1a1cfa06349c1b311b9850d8fbb25d8a057
BertramLambert <enzo.wago.yan@gmail.com>
2026-01-22 17:43:37 +0800

    Python Flask Vue Web

---
+++ Codesys/CC100/03 Python/_Python_Faskapi_Web/Python File/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>這是 cc100 容器跑出來的網頁！</h1><p>Python 版本：3.14.2</p>"
+
+if __name__ == "__main__":
+    # 必須監聽 0.0.0.0 才能從容器外存取
+    app.run(host='0.0.0.0', port=5000)
